d2c9ceaa1caad1f38209f3839df565a73804d909
gilmar123-san <69655786+gilmar123-san@users.noreply.github.com>
2022-04-20 23:08:25 -0300

    inserindo novos exercícios

--- DesenhandoOvaisRetangulos/src/ChamadaExercicioCap5.java
+++ DesenhandoOvaisRetangulos/src/ChamadaExercicioCap5.java
@@ -8,16 +8,16 @@ public class ChamadaExercicioCap5 {
 		Scanner input = new Scanner(System.in);
 
 		/*
-		 * 5.1 Desenhe 12 cÌrculos concÍntricos no centro de um JPanel (Figura 5.29). O
-		 * cÌrculo mais interno deve ter um raio de 10 pixels e cada cÌrculo sucessivo
+		 * 5.1 Desenhe 12 c√≠rculos conc√™ntricos no centro de um JPanel (Figura 5.29). O
+		 * c√≠rculo mais interno deve ter um raio de 10 pixels e cada c√≠rculo sucessivo
 		 * deve ter um raio de 10 pixels maior que o anterior. Comece localizando o
-		 * centro do JPanel. Para obter o canto superior esquerdo de um cÌrculo, mova-se
+		 * centro do JPanel. Para obter o canto superior esquerdo de um c√≠rculo, mova-se
 		 * um raio para cima e um raio para a esquerda a partir do centro. A largura e a
-		 * altura do ret‚ngulo delimitador tÍm o mesmo di‚metro do cÌrculo (isto È, duas
+		 * altura do ret√¢ngulo delimitador t√™m o mesmo di√¢metro do c√≠rculo (isto √©, duas
 		 * vezes o raio).
 		 * 
-		 * 5.2 Modifique a Quest„o 5.16 no final dos exercÌcios do capÌtulo para ler a
-		 * entrada utilizando di·logos e exibir o gr·fico de barras usando ret‚ngulos de
+		 * 5.2 Modifique a Quest√£o 5.16 no final dos exerc√≠cios do cap√≠tulo para ler a
+		 * entrada utilizando di√°logos e exibir o gr√°fico de barras usando ret√¢ngulos de
 		 * comprimentos variados.
 		 * 
 		 * String escolha = JOptionPane.
@@ -25,7 +25,7 @@ public class ChamadaExercicioCap5 {
 		 * ); int decisao = Integer.parseInt(escolha);
 		 * 
 		 * int [] retornoCliente = new int [5]; int contador=0; do { String escolha =
-		 * JOptionPane.showInputDialog("Informe 5 n˙meros entre 1 e 30:"); int decisao =
+		 * JOptionPane.showInputDialog("Informe 5 n√∫meros entre 1 e 30:"); int decisao =
 		 * Integer.parseInt(escolha); retornoCliente[contador] = decisao;
 		 * }while(++contador < 5);
 		 * 
@@ -36,51 +36,51 @@ public class ChamadaExercicioCap5 {
 		 * tela.setDefaultCloseOperation(tela.EXIT_ON_CLOSE); tela.add(painel);
 		 * tela.setSize(300, 300); tela.setVisible(true);
 		 * 
-		 * 5.3 Escreva uma instruÁ„o Java ou um conjunto de instruÁıes Java para
-		 * realizar cada uma das seguintes tarefas: a) Some os inteiros Ìmpares entre 1
-		 * e 99 utilizando uma instruÁ„o for. Assuma que as vari·veis de inteiro sum e
+		 * 5.3 Escreva uma instru√ß√£o Java ou um conjunto de instru√ß√µes Java para
+		 * realizar cada uma das seguintes tarefas: a) Some os inteiros √≠mpares entre 1
+		 * e 99 utilizando uma instru√ß√£o for. Assuma que as vari√°veis de inteiro sum e
 		 * count foram declaradas. int sum = 0; for(int count = 1 ; count < 100 ;
 		 * count++) { if((count%2) != 0) { sum += count;
-		 * System.out.println("N˙mero impar: "+count); System.out.println("Soma: "+sum);
+		 * System.out.println("N√∫mero impar: "+count); System.out.println("Soma: "+sum);
 		 * } }
 		 * 
-		 * b) Calcule o valor de 2.5 elevado ‡ potÍncia de 3, utilizando o mÈtodo pow.
+		 * b) Calcule o valor de 2.5 elevado √† pot√™ncia de 3, utilizando o m√©todo pow.
 		 * 
 		 * double resultado = Math.pow(2.5,3);
 		 * System.out.println("Resultado: "+resultado);
 		 * 
-		 * c) Imprima os inteiros de 1 a 20, utilizando um loop while e a vari·vel
-		 * contadora i. Assuma que a vari·vel i foi declarada, mas n„o foi inicializada.
-		 * Imprima apenas cinco inteiros por linha. [Dica: utilize o c·lculo i % 5.
-		 * Quando o valor dessa express„o for 0, imprima um caractere de nova linha;
-		 * caso contr·rio, imprima um caractere de tabulaÁ„o. Assuma que esse cÛdigo È
-		 * um aplicativo. Utilize o mÈtodo System.out.println () para imprimir o
-		 * caractere de nova linha, e utilize o mÈtodo System.out.print ('\t') para
-		 * imprimir o caractere de tabulaÁ„o.]
+		 * c) Imprima os inteiros de 1 a 20, utilizando um loop while e a vari√°vel
+		 * contadora i. Assuma que a vari√°vel i foi declarada, mas n√£o foi inicializada.
+		 * Imprima apenas cinco inteiros por linha. [Dica: utilize o c√°lculo i % 5.
+		 * Quando o valor dessa express√£o for 0, imprima um caractere de nova linha;
+		 * caso contr√°rio, imprima um caractere de tabula√ß√£o. Assuma que esse c√≥digo √©
+		 * um aplicativo. Utilize o m√©todo System.out.println () para imprimir o
+		 * caractere de nova linha, e utilize o m√©todo System.out.print ('\t') para
+		 * imprimir o caractere de tabula√ß√£o.]
 		 * 
 		 * int i = 1; while(i <= 20) { if(i % 5 == 1 && i != 1) { System.out.println();
 		 * } System.out.print(i+","); i++; }
 		 * 
-		 * d) Repita a parte (c) utilizando uma instruÁ„o for.
+		 * d) Repita a parte (c) utilizando uma instru√ß√£o for.
 		 * 
 		 * for(int i = 1; i <=* 20;i++) { if(i % 5 == 1 && i != 1) {
 		 * System.out.println(); } System.out.print(i+","); }
 		 * 
 		 * 
 		 * 5.11 (Localize o menor valor) Escreva um aplicativo que localiza o menor de
-		 * v·rios n˙meros inteiros. Suponha que o primeiro valor lido especifica o
-		 * n˙mero de valores a serem inseridos pelo usu·rio.
+		 * v√°rios n√∫meros inteiros. Suponha que o primeiro valor lido especifica o
+		 * n√∫mero de valores a serem inseridos pelo usu√°rio.
 		 * 
-		 * System.out.println("informe quantos n˙meros ser„o inseridos:"); int contador
+		 * System.out.println("informe quantos n√∫meros ser√£o inseridos:"); int contador
 		 * = input.nextInt(); int[] vetor = new int[contador]; int aux = 0; contador =
-		 * 0; System.out.println("Gerando n˙meros aleatoriamente..."+"\n"); SecureRandom
+		 * 0; System.out.println("Gerando n√∫meros aleatoriamente..."+"\n"); SecureRandom
 		 * random = new SecureRandom(); while (contador < vetor.length) {
 		 * vetor[contador] = random.nextInt(); if (contador == 0) { aux =
 		 * vetor[contador]; } else if (vetor[contador] < aux) { aux = vetor[contador]; }
-		 * contador++; } System.out.println("O menor n˙mero È: "+aux);
+		 * contador++; } System.out.println("O menor n√∫mero √©: "+aux);
 		 * 
-		 * 5.12 (Calculando o produto de n˙meros inteiros Ìmpares) Escreva um aplicativo
-		 * que calcula o produto dos n˙meros inteiros Ìmpares de 1 a 15.
+		 * 5.12 (Calculando o produto de n√∫meros inteiros √≠mpares) Escreva um aplicativo
+		 * que calcula o produto dos n√∫meros inteiros √≠mpares de 1 a 15.
 		 * 
 		 * 
 		 * int i = 0; int res = 0; for (i = 1; i <= 15; i++) { if (i % 2 != 0) {
@@ -89,10 +89,10 @@ public class ChamadaExercicioCap5 {
 		 * 
 		 * 5.13 (Fatoriais) Fatoriais costumam ser utilizados em problemas de
 		 * probabilidade. O fatorial de um inteiro positivo n (escrito como n! e
-		 * pronunciado como ìfatorial de nî) È igual ao produto dos n˙meros inteiros
+		 * pronunciado como ‚Äúfatorial de n‚Äù) √© igual ao produto dos n√∫meros inteiros
 		 * positivos de 1 a n. Escreva um aplicativo que calcula os fatoriais de 1 a 20.
 		 * Utilize o tipo long. Exiba os resultados em formato tabular. Que dificuldade
-		 * poderia impedir vocÍ de calcular o fatorial de 100?
+		 * poderia impedir voc√™ de calcular o fatorial de 100?
 		 * 
 		 * double valor7 = 0.0; float valor6 = 0.0f; long valor5 = 0l; int valor4 = 0;
 		 * short valor3 = 0; char valor2 = 0; boolean valor1 = true;
@@ -108,33 +108,33 @@ public class ChamadaExercicioCap5 {
 		 * 7%, 8%, 9% e 10%. Utilize um loop for para variar a taxa de juros.
 		 * 
 		 * 
-		 * double amount; // quantia em depÛsito ao fim de cada ano double principal =
+		 * double amount; // quantia em dep√≥sito ao fim de cada ano double principal =
 		 * 0; // quantidade inicial antes dos juros double rate = 0; // taxa de juros
 		 * 
 		 * System.out.println("Informe o valor a ser calculado:"); principal =
 		 * input.nextInt();
 		 * 
-		 * // exibe cabeÁalhos System.out.printf("%s%20s %n", "Year",
+		 * // exibe cabe√ßalhos System.out.printf("%s%20s %n", "Year",
 		 * "Amount on deposit");
 		 * 
 		 * for (int i = 5; i < 10; i++) { rate = (double)i / 100; // calcula quantidade
-		 * de depÛsito para cada um dos dez anos for (int year = 1; year <= 10; ++year)
+		 * de dep√≥sito para cada um dos dez anos for (int year = 1; year <= 10; ++year)
 		 * { // calcula nova quantidade durante ano especificado amount = principal *
 		 * Math.pow(1.0 + rate, year);
 		 * 
-		 * // exibe o ano e a quantidade ˇ System.out.printf("%4d%,20.2f%n", year,
+		 * // exibe o ano e a quantidade √ø System.out.printf("%4d%,20.2f%n", year,
 		 * amount); } System.out.println(); }
 		 * 
-		 * 5.15 (Programa para impress„o de tri‚ngulos) Escreva um aplicativo que exibe
-		 * os seguintes padrıes separadamente, um embaixo do outro. Utilize loops for
-		 * para gerar os padrıes. Todos os asteriscos (*) devem ser impressos por uma
-		 * ˙nica instruÁ„o na forma System.out. print('*'); o que faz com que os
-		 * asteriscos sejam impressos lado a lado. Uma instruÁ„o na forma
-		 * System.out.println(); pode ser utilizada para mover-se para a prÛxima linha.
-		 * Uma instruÁ„o na forma System.out.print(' '); pode ser utilizada para exibir
-		 * um espaÁo para os ˙ltimos dois padrıes. N„o deve haver outras instruÁıes de
-		 * saÌda no programa. [Dica: os dois ˙ltimos padrıes requerem que cada linha
-		 * inicie com um n˙mero adequado de espaÁos em branco.] (a) (b) (c) (d)
+		 * 5.15 (Programa para impress√£o de tri√¢ngulos) Escreva um aplicativo que exibe
+		 * os seguintes padr√µes separadamente, um embaixo do outro. Utilize loops for
+		 * para gerar os padr√µes. Todos os asteriscos (*) devem ser impressos por uma
+		 * √∫nica instru√ß√£o na forma System.out. print('*'); o que faz com que os
+		 * asteriscos sejam impressos lado a lado. Uma instru√ß√£o na forma
+		 * System.out.println(); pode ser utilizada para mover-se para a pr√≥xima linha.
+		 * Uma instru√ß√£o na forma System.out.print(' '); pode ser utilizada para exibir
+		 * um espa√ßo para os √∫ltimos dois padr√µes. N√£o deve haver outras instru√ß√µes de
+		 * sa√≠da no programa. [Dica: os dois √∫ltimos padr√µes requerem que cada linha
+		 * inicie com um n√∫mero adequado de espa√ßos em branco.] (a) (b) (c) (d)
 		 * ********** ********** * ********* ********* ** ******** ******** *** *******
 		 * ******* **** ****** ****** ***** ***** ***** ****** **** **** ******* *** ***
 		 * ******** ** ** ********* * * **********
@@ -150,17 +150,17 @@ public class ChamadaExercicioCap5 {
 		 * if (i < 10) { System.out.printf("%" + (i - 10) + "s", " "); } for (int j = 1;
 		 * j <= i; j++) { System.out.print("*"); } System.out.println(); }
 		 * 
-		 * 5.16 (Gr·fico de barras do programa de impress„o) Uma aplicaÁ„o interessante
-		 * dos computadores È exibir diagramas e gr·ficos de barras. Escreva um
-		 * aplicativo que leia cinco n˙meros entre 1 e 30. Para cada n˙mero que È lido,
-		 * seu programa deve exibir o mesmo n˙mero de asteriscos adjacentes. Por
-		 * exemplo, se seu programa lÍ o n˙mero 7, ele deve exibir *******. Exiba as
-		 * barras dos asteriscos depois de ler os cinco n˙meros.
+		 * 5.16 (Gr√°fico de barras do programa de impress√£o) Uma aplica√ß√£o interessante
+		 * dos computadores √© exibir diagramas e gr√°ficos de barras. Escreva um
+		 * aplicativo que leia cinco n√∫meros entre 1 e 30. Para cada n√∫mero que √© lido,
+		 * seu programa deve exibir o mesmo n√∫mero de asteriscos adjacentes. Por
+		 * exemplo, se seu programa l√™ o n√∫mero 7, ele deve exibir *******. Exiba as
+		 * barras dos asteriscos depois de ler os cinco n√∫meros.
 		 * 
 		 * 
 		 * int[] vetor = new int[5]; int i = 0; SecureRandom numberRandom = new
 		 * SecureRandom();
-		 * System.out.println("Gerando 5 n˙meros aleatÛrios entre 1 e 30:"); while (i <
+		 * System.out.println("Gerando 5 n√∫meros aleat√≥rios entre 1 e 30:"); while (i <
 		 * vetor.length) { vetor[i] = 1 + numberRandom.nextInt(30); i++; };
 		 * 
 		 * for(i = 0; i < vetor.length; i++) { for(int j = 0; j < vetor[i]; j++) {
@@ -168,15 +168,167 @@ public class ChamadaExercicioCap5 {
 		 * System.out.println(); }
 		 * 
 		 * 5.17 (Calculando vendas) Um varejista on-line vende cinco produtos cujos
-		 * preÁos no varejo s„o como a seguir: produto 1, US$ 2,98; produto 2, US$ 4,50;
+		 * pre√ßos no varejo s√£o como a seguir: produto 1, US$ 2,98; produto 2, US$ 4,50;
 		 * produto 3, US$ 9,98; produto 4, US$ 4,49 e produto 5, US$ 6,87. Escreva um
-		 * aplicativo que leia uma sÈrie de pares de n˙meros como segue: a) n˙mero de
-		 * produto b) quantidade vendida Seu programa deve utilizar uma instruÁ„o switch
-		 * para determinar o preÁo de varejo de cada produto. VocÍ deve calcular e
+		 * aplicativo que leia uma s√©rie de pares de n√∫meros como segue: a) n√∫mero de
+		 * produto b) quantidade vendida Seu programa deve utilizar uma instru√ß√£o switch
+		 * para determinar o pre√ßo de varejo de cada produto. Voc√™ deve calcular e
 		 * exibir o valor de varejo total de todos os produtos vendidos. Utilize um loop
 		 * controlado por sentinela para determinar quando o programa deve parar o loop
 		 * e exibir os resultados finais.
+		 * 
+		 * 
+		 * double totalVendido = 0; int sentinela = 1;
+		 * 
+		 * do { System.out.
+		 * println("Informe um produto do estoque entre 1 e 5 (0 - para finalizar):");
+		 * int produto = input.nextInt(); if(produto != 0) {
+		 * System.out.println("Informe a quantidade vendida:"); int quantidade =
+		 * input.nextInt();
+		 * 
+		 * System.out.println("Informe o pre√ßo unit√°rio:"); double preco =
+		 * input.nextDouble();
+		 * 
+		 * totalVendido = vendeProduto(produto, quantidade, preco); }else { sentinela =
+		 * 0; }
+		 * 
+		 * } while (sentinela != 0); if(totalVendido != 0) {
+		 * System.out.printf("Valor total da venda: %.2f", totalVendido); }else
+		 * if(totalVendido==0){ System.out.println("Nenhum produto inserido!"); }
+		 *
+		 *
+		 * 
+		 * 5.18 (Programa de juros compostos modificado) Modifique o aplicativo na
+		 * Figura 5.6 para utilizar apenas inteiros para calcular os juros compostos.
+		 * [Dica: trate todas as quantidades monet√°rias como n√∫meros inteiros em
+		 * centavos. Ent√£o, divida o resultado em suas partes d√≥lar e centavos
+		 * utilizando as opera√ß√µes divis√£o e resto, respectivamente. Insira uma v√≠rgula
+		 * entre as partes d√≥lar e centavos.
+		 * 
+		 * double resultado; double aumentoAnual = 0.05; // taxa de juros String
+		 * restoDeCentavos; int ajusteValor = 0; int deposito = 1000; // quantidade
+		 * inicial antes dos juros int dolar = 0; int centavos = 0;
+		 * 
+		 * // exibe cabe√ßalhos System.out.printf("%s%20s %n", "Year",
+		 * "Amount on deposit");
+		 * 
+		 * // calcula quantidade de dep√≥sito para cada um dos dez anos for (int year =
+		 * 1; year <= 10; ++year) {
+		 * 
+		 * resultado = deposito * Math.pow(1 + aumentoAnual, year); dolar = (int)
+		 * resultado / 100; if (dolar < 100) { centavos = (int) resultado % 100;
+		 * restoDeCentavos = String.format("%.2f", resultado % 100); if
+		 * (restoDeCentavos.length() < 5) { ajusteValor +=
+		 * Integer.parseInt(restoDeCentavos.substring(2, restoDeCentavos.length()));
+		 * if(ajusteValor==100) { centavos++; } } } // int numeroReal=
+		 * Integer.parseInt(stringDeDados);
+		 * 
+		 * System.out.printf("%4d%,20d,%d%n", year, dolar, centavos); }
+		 *
+		 * 5.19 Suponha que i = 1, j = 2, k = 3 e m = 2. O que cada uma das seguintes
+		 * instru√ß√µes imprime? a) System.out.println(i == 1) = true; b)
+		 * System.out.println(j == 3) = false; c) System.out.println((i >= 1) && (j <
+		 * 4)) = true; d) System.out.println((m <= 99) & (k < m)) = false; e)
+		 * System.out.println((j >= i) || (k == m)) = true; f) System.out.println((k + m
+		 * < j) | (3 - j >= k)) = false; g) System.out.println(!(k > m)) = false;
+		 * 
+		 * 5.20 (Calculando o valor de pi) Calcule o valor de pi a partir de uma s√©rie
+		 * infinita Imprima uma tabela que mostre o valor aproximado de pi calculando os
+		 * 200.000 primeiros termos dessa s√©rie. Quantos termos voc√™ tem de utilizar
+		 * antes de primeiro obter um valor que come√ßa com 3,14159?
+		 * 
+		 * 
+		 * final double PRINCIPAL = 4; double pi = PRINCIPAL; boolean calcular = true;
+		 * int contador = 0;
+		 * 
+		 * System.out.printf("Contagem Valor calculado %n%8d %.5f %n", contador, pi);
+		 * for (int i = 1; i <= 500000; i++) { if (i > 2 && i % 2 != 0) { if (calcular)
+		 * { pi -= (PRINCIPAL / i); calcular = false; contador++;
+		 * System.out.printf(" %n%8d %.5f %n", contador, pi); } else { pi += (PRINCIPAL
+		 * / i); calcular = true; contador++; System.out.printf("%n%8d %.5f %n",
+		 * contador, pi); } }
+		 * 
+		 * String aux = String.format("%.5f", pi);
+		 * 
+		 * if (aux.equals("3,14159")) { System.out.println("Fim da contagem!!"); break;
+		 * } }
+		 *
+		 *
+		 * 
+		 * 5.21 (Triplos de Pit√°goras) Um tri√¢ngulo ret√¢ngulo pode ter lados cujos
+		 * comprimentos s√£o todos inteiros. O conjunto de tr√™s valores inteiros para os
+		 * comprimentos dos lados de um tri√¢ngulo ret√¢ngulo √© chamado de triplo de
+		 * Pit√°goras. Os comprimentos dos tr√™s lados devem satisfazer a rela√ß√£o de que a
+		 * soma dos quadrados de dois dos lados √© igual ao quadrado da hipotenusa.
+		 * Escreva um aplicativo para exibir uma tabela dos triplos de Pit√°goras para
+		 * side1, side2 e hypotenuse, todos n√£o maiores que 500. Utilize um loop for
+		 * triplamente aninhado que tenta todas as possibilidades. Esse √© um m√©todo de
+		 * computa√ß√£o de ‚Äúfor√ßa bruta‚Äù. Voc√™ aprender√° nos cursos de ci√™ncia da
+		 * computa√ß√£o mais avan√ßados que para muitos problemas interessantes n√£o h√° uma
+		 * abordagem algor√≠tmica conhecida al√©m do uso de for√ßa bruta absoluta.
+		 * 
+		 * 
+		 * System.out.println("Calculando valores de catetos e hipotenusa!");
+		 * System.out.printf("%5s / %5s / %5s %n","Cateto1","Cateto 2","hipotenusa");
+		 * for(int lado1 = 1; lado1 <= 500; lado1++) { for(int lado2 = 1; lado2 <= 500;
+		 * lado2++) { for(int hipotenusa = 1; hipotenusa <= 500; hipotenusa++) {
+		 * if((hipotenusa*hipotenusa) == (lado1*lado1)+(lado2*lado2)) {
+		 * System.out.printf("%7d %7d %9d%n",lado1, lado2, hipotenusa); } } } }
+		 * 
+		 * 5.22 (Programa de impress√£o de tri√¢ngulos modificado) Modifique a Quest√£o
+		 * 5.15 para combinar seu c√≥digo dos quatro tri√¢ngulos de asteriscos separados,
+		 * de modo que todos os quatro padr√µes sejam impressos lado a lado. [Dica: fa√ßa
+		 * uso inteligente de loops for aninhados.]
 		 */
-		
+
+		for (int i = 1; i <= 10; i++) {
+			
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+				if (j + 1 > i) {
+					for (int a = 1; a <= 11 - i; a++) {
+						System.out.printf("%s", " ");
+					}
+				}
+			}
+			
+			for (int j = 10 - (i - 1); j >= 1; j--) {
+				System.out.print("*");
+				if (j <= 1) {
+					for (int a = 1; a <= i; a++) {
+						System.out.printf("%s", " ");
+					}
+				}
+			}
+			
+			for (int j = 10 - (i - 1); j >= 1; j--) {
+				if (j == 10-(i-1)) {
+					for (int a = 1; a <= i-1; a++) {	
+						System.out.print(" ");
+					}
+				}
+				System.out.print("*");
+			}
+			
+			for (int j = 10 - (i - 1); j >= 1; j--) {
+				System.out.print(" ");
+				if (j <= 1) {
+					for (int a = 1; a <= i; a++) {
+						System.out.print("*");
+					}
+				}
+			}
+			System.out.println();
+		}
+
 	}
+
+	/*
+	 * public static double vendeProduto(int produto, int quantidade, double preco)
+	 * { double totalVendido = 0; switch (produto) { case 1: totalVendido +=
+	 * quantidade * preco; break; case 2: totalVendido += quantidade * preco; break;
+	 * case 3: totalVendido += quantidade * preco; break; case 4: totalVendido +=
+	 * quantidade * preco; break; case 5: totalVendido += quantidade * preco; break;
+	 * } return totalVendido; }
+	 */
 }
